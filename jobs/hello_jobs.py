e9d4928c7911d259da28b6b4c8572e79783a1b49
Mahmoed Hassan <mahmoedhassan1@gmail.com>
2025-04-05 14:14:59 +0200

    Changed config of hellp_jobs to version 1.6.7

--- jobs/hello_jobs.py
+++ jobs/hello_jobs.py
@@ -1,8 +1,10 @@
-from nautobot.apps.jobs import Job
+# from nautobot.apps.jobs import Job Version 3.1 
+from nautobot.extras.jobs import Job
 
 class HelloJobs(Job):
     class Meta:
         name = "Hello jobs from Git repo"
 
     def run(self):
-        self.logger.debug("this is from the Git repo.")
+        self.log_info("this is from the Git repo.")
+        # self.logger__("this is from the Git repo.")
