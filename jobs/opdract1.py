cbf6852c4a9a30179815efab38304ad1c49bd9c1
Mahmoed Hassan <mahmoedhassan1@gmail.com>
2025-04-05 14:29:23 +0200

    Testing with coa version

--- jobs/opdract1.py
+++ jobs/opdract1.py
@@ -1,16 +1,27 @@
-from nautobot.extras.jobs import Job
-from nautobot.extras.jobs import *
-# import pandas as pd
+from nautobot.extras.jobs import Job, FileVar
+# from nautobot.extras.jobs import *
+import pandas as pd
 import io
 import os
 
-name = "First job 4"
+name = "First job 5"
 
 
 class TestingJob(Job):
     class Meta:
-        name = "Test job 4 Layla"
-        description = "This is an test 4"
+        name = "Test job 5 Layla"
+        description = "This is an test 5"
+    excel_file = FileVar()
 
     def run(self, data, commit):
+        excel_file = data.get("excel_file", None)
         self.log_info("Het werkt")
+        if not excel_file:
+            self.log_info("The excel file path is invalid of the file does not exists")
+        try:
+            df = pd.read_excel(excel_file)
+            for index, row in df.iterrows():
+                self.log_info(index, row)
+            self.log_info(f"First 5 rows of the excel file:\n{df.head()}")
+        except Exception as e:
+            self.log_info(f"Error reading the file str{e}")
